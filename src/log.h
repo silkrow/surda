0940e6315e9abfa7cd2aab1354e82c7f32bbf61b
silkrow <si1krow@outlook.com>
2022-01-01 00:16:40 +0800

    Half implemented add_plan function, and happy new yeargit add .

--- src/log.h
+++ src/log.h
@@ -33,7 +33,11 @@ typedef struct Log{
 	int open;
 	char* name;
 	FILE* logf;
-	int t; //  Indicating which type this log is.
+	int t; //  Indicating which type this log is(weekly or template).
+	/* The following three are like stack elements. */
+	int duration;
+	int start;
+	int end;
 } Log;
 
 /* The Table structure should contain a file of txt form to display the table,
@@ -92,7 +96,6 @@ int count_args(char** args){
 * Description:      Reads in information and add plan to the log file.
 *****************************************************************************/
 int add_plan (char** args, Log* log){
-	int start, end, duration;
 	int day;
 
 	if (!touch_log(log)) return 1;
@@ -102,7 +105,7 @@ int add_plan (char** args, Log* log){
 		case 3:
 			day = now->tm_wday;
 			if (time_convert(args[1], day) == -1 || time_convert(args[2], day) == -1 ||
-					(start = time_convert(args[1], day)) >= (end = time_convert(args[2], day))){
+					(log->start = time_convert(args[1], day)) >= (log->end = time_convert(args[2], day))){
 				printf("surda: Invalid syntax for add!\n"
 					   "       Use add time1 time2 [day]\n"
 					   "       where time should be in format of 24h, with preceding 0 for hours < 10.\n"
@@ -111,18 +114,22 @@ int add_plan (char** args, Log* log){
 					   "       Type \"help\" for help.\n");
 			}
 			else{
-				duration = end - start;
-				log->logf = fopen(log->name, "r+");
+				log->duration = log->end - log->start;
+				if (NULL == (log->logf = fopen(log->name, "r+"))){
+					printf("surda: Adding failed, please check if the log file exists.\n");
+					return 1;
+				}
+
 				fseek(log->logf, 0, SEEK_SET);// Preset the pointer in the file.
 				
 				/* Now set the pointer in the file to the start line. */
-				if (!jump_line(log->logf, start)){
+				if (!jump_line(log->logf, log->start)){
 					printf("surda: Adding failed, something's wrong with your log file!!\n");
 					fclose(log->logf);
 					return 1;
 				}
 				else{
-					for (int i = 0; i < duration; ++i){
+					for (int i = 0; i < log->duration; ++i){
 						if (fgetc(log->logf) != '0'){
 							printf("There're already plans in the period!\n");
 							fclose(log->logf);
@@ -130,11 +137,10 @@ int add_plan (char** args, Log* log){
 						}
 						jump_line(log->logf, 1);
 					}
-					
-					fputs("here i am!!!", log->logf);
 
 					/* Now it's checked to be fine. */
 					printf("Please now enter the plan script, hit enter only when finished!\n");				
+
 					fclose(log->logf);
 					return 2;
 				}
@@ -143,6 +149,54 @@ int add_plan (char** args, Log* log){
 
 		/* time1 time2 day*/
 		case 4:
+			day = *args[3] - '0';
+			if (time_convert(args[1], day) == -1 || time_convert(args[2], day) == -1 ||
+					day > 6 || day < 0 ||
+					(log->start = time_convert(args[1], day)) >= (log->end = time_convert(args[2], day))){
+				printf("surda: Invalid syntax for add!\n"
+					   "       Use add time1 time2 [day]\n"
+					   "       where time should be in format of 24h, with preceding 0 for hours < 10.\n"
+					   "       day should be a number of 0-6, 0 for Sunday.\n"
+					   "       example:     add 08:00 24:00 4\n"
+					   "       Type \"help\" for help.\n");
+			}
+			else{
+				/* Here insert a tiny warning. */
+				if (day < now->tm_wday)
+					printf("surda warning: You're adding plans to day before today!\n");
+
+				log->duration = log->end - log->start;
+				if (NULL == (log->logf = fopen(log->name, "r+"))){
+					printf("surda: Adding failed, please check if the log file exists.\n");
+					return 1;
+				}
+
+				fseek(log->logf, 0, SEEK_SET);// Preset the pointer in the file.
+				
+				/* Now set the pointer in the file to the start line. */
+				if (!jump_line(log->logf, log->start)){
+					printf("surda: Adding failed, something's wrong with your log file!!\n");
+					fclose(log->logf);
+					return 1;
+				}
+				else{
+					for (int i = 0; i < log->duration; ++i){
+						if (fgetc(log->logf) != '0'){
+							printf("There're already plans in the period!\n");
+							fclose(log->logf);
+							return 1;
+						}
+						jump_line(log->logf, 1);
+					}
+
+					/* Now it's checked to be fine. */
+					printf("Please now enter the plan script, hit enter only when finished!\n");
+
+					fclose(log->logf);
+					return 2;
+				}
+			}
+
 			return 1;
 
 		default:
@@ -165,8 +219,18 @@ int add_plan (char** args, Log* log){
 * Description:      Simply copy the string input to the log file.
 *****************************************************************************/
 int add_plan_str(char** args, Log* log){
-	
+	/* Warning, here it's assumed that the log file is opened during add_plan,
+	 * and the file pointer should be at the specific place where plan should
+	 * be plugged in. */
+	if (NULL == (log->logf = fopen(log->name, "r+"))){
+		printf("surda: Adding failed, please check if the log file exists.\n");
+		return 1;
+	}
 
+	for (int i = 0; i < log->duration - 1; ++i){
+	}
+
+	fclose(log->logf);
 	return 1;	
 }
 
